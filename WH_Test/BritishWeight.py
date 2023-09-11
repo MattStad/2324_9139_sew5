b62101fd0dabf9a88f2d1defda851463ba946bd3
Matthias Stadlinger <9139@htl.rennweg.at>
2023-09-11 15:10:11 +0200

    fertig

---
+++ WH_Test/BritishWeight.py
@@ -0,0 +1,23 @@
+class BritishWeight:
+    """ Doctest
+    >>> b1 = BritishWeight(13)
+    >>> b2 = BritishWeight(16)
+    >>> print(b1)
+    13 lb
+    >>> print(b2)
+    1 st 2 lb
+    >>> b3 = b1 + b2
+    >>> print(b3)
+    2 st 1 lb
+    """
+    def __init__(self,pound):
+        self.pound=pound
+
+    def __str__(self):
+        if self.pound//14==0:
+            return str(self.pound)+" lb"
+        else:
+            return str(self.pound//14) +" st "+str(self.pound%14)+" lb"
+
+    def __add__(self, other):
+        return BritishWeight(self.pound+other.pound)
